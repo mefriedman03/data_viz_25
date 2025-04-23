e99540e9967fa857937be28f88f62e5fcaa58ab3
mefriedman03 <158083250+mefriedman03@users.noreply.github.com>
2025-04-22 23:24:21 -0400

    hi

--- charts js data viz/chartsjsviz3/javacript.js
+++ charts js data viz/chartsjsviz3/javacript.js
@@ -6,53 +6,56 @@ var ctx = document.getElementById('myChart').getContext('2d');
                 data: [
                   {x: 1, y: 1},
                 ],
-                label: "Korbin ALbert",
+                label: "Korbin Albert",
                 borderColor: "#3e95cd",
-                backgroundColor: "rgb(255,165,0,0.1)",
+                backgroundColor: "rgb(62,149,205,0.1)",
                 borderWidth:2,
                
               }, { 
                 data: [
-                    {x: 0, y: 2},
-                  ],
-                  label: "Sam Coffey",
-                  borderColor: "#3e95cd",
-                  backgroundColor: "rgb(255,165,0,0.1)",
-                  borderWidth:2,
+                  {x: 0, y: 2},
+                ],
+                label: "Sam Coffey",
+                borderColor: "#3cba9f",
+                backgroundColor: "rgb(60,186,159,0.1)",
+                borderWidth:2,
                 
               }, { 
                 data: [
-                    {x: 2, y: 0},
-                    ],
-                    label: "Tierna Davidson",
-                    borderColor: "#3e95cd",
-                    backgroundColor:"rgb(255,165,0,0.1)",
-                    borderWidth:2,
+                {x: 2, y: 0},
+                ],
+                label: "Tierna Davidson",
+                borderColor: "#ffa500",
+                backgroundColor:"rgb(255,165,0,0.1)",
+                borderWidth:2,
                 
-              }, { 
+              },
+              { 
                 data: [
-                    {x: 1, y: 1},
-                    ],
-                    label: "Crystal Dunn",
-                    borderColor: "#3e95cd",
-                    backgroundColor:"rgb(255,165,0,0.1)",
-                    borderWidth:2,
+                {x: 1, y: 1},
+                ],
+                label: "Crystal Dunn",
+                borderColor: "#ffa500",
+                backgroundColor:"rgb(255,165,0,0.1)",
+                borderWidth:2,
                 
-              },{ 
+              },
+              { 
                 data: [
-                    {x: 0, y: 1},
-                    ],
-                    label: "Emily Fox",
-                    borderColor: "#3e95cd",
-                    backgroundColor:"rgb(255,165,0,0.1)",
-                    borderWidth:2,
+                {x: 0, y: 1},
+                ],
+                label: "Emily Fox",
+                borderColor: "#ffa500",
+                backgroundColor:"rgb(255,165,0,0.1)",
+                borderWidth:2,
                 
-              },{ 
+              },
+              { 
                 data: [
                 {x: 2, y: 0},
                 ],
                 label: "Naomi Girma",
-                borderColor: "#3e95cd",
+                borderColor: "#ffa500",
                 backgroundColor:"rgb(255,165,0,0.1)",
                 borderWidth:2,
                 
@@ -62,7 +65,7 @@ var ctx = document.getElementById('myChart').getContext('2d');
                 {x: 5, y: 3},
                 ],
                 label: "Lindsey Horan",
-                borderColor: "#3e95cd",
+                borderColor: "#ffa500",
                 backgroundColor:"rgb(255,165,0,0.1)",
                 borderWidth:2,
                 
@@ -72,7 +75,7 @@ var ctx = document.getElementById('myChart').getContext('2d');
                 {x: 0, y: 2},
                 ],
                 label: "Casey Kreuger",
-                borderColor: "#3e95cd",
+                borderColor: "#ffa500",
                 backgroundColor:"rgb(255,165,0,0.1)",
                 borderWidth:2,
                 
@@ -82,7 +85,7 @@ var ctx = document.getElementById('myChart').getContext('2d');
                 {x: 0, y: 3},
                 ],
                 label: "Rose Lavelle",
-                borderColor: "#3e95cd",
+                borderColor: "#ffa500",
                 backgroundColor:"rgb(255,165,0,0.1)",
                 borderWidth:2,
                 
@@ -92,7 +95,7 @@ var ctx = document.getElementById('myChart').getContext('2d');
                 {x: 0, y: 1},
                 ],
                 label: "Catarina Macario",
-                borderColor: "#3e95cd",
+                borderColor: "#ffa500",
                 backgroundColor:"rgb(255,165,0,0.1)",
                 borderWidth:2,
                 
@@ -102,7 +105,7 @@ var ctx = document.getElementById('myChart').getContext('2d');
                 {x: 2, y: 1},
                 ],
                 label: "Alex Morgan",
-                borderColor: "#3e95cd",
+                borderColor: "#ffa500",
                 backgroundColor:"rgb(255,165,0,0.1)",
                 borderWidth:2,
                 
@@ -112,7 +115,7 @@ var ctx = document.getElementById('myChart').getContext('2d');
                 {x: 2, y: 1},
                 ],
                 label: "Jenna Nighswonger",
-                borderColor: "#3e95cd",
+                borderColor: "#ffa500",
                 backgroundColor:"rgb(255,165,0,0.1)",
                 borderWidth:2,
                 
@@ -122,7 +125,7 @@ var ctx = document.getElementById('myChart').getContext('2d');
                 {x: 0, y: 2},
                 ],
                 label: "Midge Purce",
-                borderColor: "#3e95cd",
+                borderColor: "#ffa500",
                 backgroundColor:"rgb(255,165,0,0.1)",
                 borderWidth:2,
                 
@@ -132,7 +135,7 @@ var ctx = document.getElementById('myChart').getContext('2d');
                 {x: 0, y: 1},
                 ],
                 label: "Yazmeen Ryan",
-                borderColor: "#3e95cd",
+                borderColor: "#ffa500",
                 backgroundColor:"rgb(255,165,0,0.1)",
                 borderWidth:2,
                 
@@ -142,7 +145,7 @@ var ctx = document.getElementById('myChart').getContext('2d');
                 {x: 1, y: 1},
                 ],
                 label: "Emma Sears",
-                borderColor: "#3e95cd",
+                borderColor: "#ffa500",
                 backgroundColor:"rgb(255,165,0,0.1)",
                 borderWidth:2,
                 
@@ -152,7 +155,7 @@ var ctx = document.getElementById('myChart').getContext('2d');
                 {x: 6, y: 1},
                 ],
                 label: "Jaedyn Shaw",
-                borderColor: "#3e95cd",
+                borderColor: "#ffa500",
                 backgroundColor:"rgb(255,165,0,0.1)",
                 borderWidth:2,
                 
@@ -162,7 +165,7 @@ var ctx = document.getElementById('myChart').getContext('2d');
                 {x: 9, y: 4},
                 ],
                 label: "Sophia Smith",
-                borderColor: "#3e95cd",
+                borderColor: "#ffa500",
                 backgroundColor:"rgb(255,165,0,0.1)",
                 borderWidth:2,
                 
@@ -172,7 +175,7 @@ var ctx = document.getElementById('myChart').getContext('2d');
                 {x: 6, y: 4},
                 ],
                 label: "Mallory Swanson",
-                borderColor: "#3e95cd",
+                borderColor: "#ffa500",
                 backgroundColor:"rgb(255,165,0,0.1)",
                 borderWidth:2,
                 
@@ -182,7 +185,7 @@ var ctx = document.getElementById('myChart').getContext('2d');
                 {x: 1, y: 0},
                 ],
                 label: "ALyssa Thompson",
-                borderColor: "#3e95cd",
+                borderColor: "#ffa500",
                 backgroundColor:"rgb(255,165,0,0.1)",
                 borderWidth:2,
                 
@@ -192,7 +195,7 @@ var ctx = document.getElementById('myChart').getContext('2d');
                 {x: 4, y: 1},
                 ],
                 label: "Lynn Williams",
-                borderColor: "#3e95cd",
+                borderColor: "#ffa500",
                 backgroundColor:"rgb(255,165,0,0.1)",
                 borderWidth:2,
                 
@@ -202,40 +205,25 @@ var ctx = document.getElementById('myChart').getContext('2d');
                 {x: 1, y: 0},
                 ],
                 label: "Lily Yohannes",
-                borderColor: "#3e95cd",
+                borderColor: "#ffa500",
                 backgroundColor:"rgb(255,165,0,0.1)",
                 borderWidth:2,
                 
               }
-
             ]
           },
           options: {
-            plugins: {
-                legend:{
-                    display:false},
-                title: {
-                    display: true,
-                    text: 'Sophia Smith and Mallory Swanson were tied for most assists in 2024, but Smith surpassed Swanson in goals',
-                    font: {
-                        size: 18
-                    }
-                },
-                subtitle: {
-                    display: true,
-                    text: 'Seven USWNNT players had at least one assist but never scored in 2024.'
-                },},
-                scales: {
+            scales: {
               y: {
                 title: {
                   display: true,
-                  text: 'Assists'
+                  text: 'Y axis title'
                 }
               },
               x: {
                   title: {
                       display: true,
-                      text: 'Goals'
+                      text: 'X axis title'
                   }
               }
             }
